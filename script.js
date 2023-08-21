a4e088e509fc6a7f9f655689fa958c54d38b69ed
kalim shaikh <117559118+KalimGithub@users.noreply.github.com>
2023-08-21 23:58:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
+	var form = document.getElementById("form1");
+    var formData = new FormData(form);
 
+    var firstname = formData.get("fname");
+    var lastname = formData.get("lname");
+
+    alert(firstname + " " + lastname);
 }
+
+
+
+
